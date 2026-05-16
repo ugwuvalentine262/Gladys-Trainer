c08800d76270a0c68ab67bb6b3051c70088654d1
Ugwu Valentine <ugwuvalentine262@gmail.com>
2026-05-15 21:38:07 -0600

    deleted some irrelvant files - they can be bundled as a single unit

--- src/mse.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2026 Ugwu Valentine Nnannaya.
- * All rights reserved.
- *
- * This project is proprietary. Unauthorized copying,
- * modification, or distribution is not permitted.
- */
-
-#include <mse.hpp>
