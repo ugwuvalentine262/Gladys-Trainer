e5aac5496712d004d1f3368c922279c3529c29f3
Ugwu Valentine <ugwuvalentine262@gmail.com>
2026-06-12 17:40:07 -0600

    upgraded algos as a result of including bit header file which is defined in c++20

--- src/io.hpp
+++ src/io.hpp
@@ -10,57 +10,43 @@
 #define IO_HPP
 
 #include <cstdint>
-#include <cstring>
+#include <bit>
 #include <fstream>
 
-inline bool is_little_endian()
+inline float reverse_bytes(float f) 
 {
-	uint16_t value = 0x0001U;
-	return *reinterpret_cast<uint8_t*>(&value) == 1;
-}
-
-inline float reverse_bytes(float f)
-{
-	uint32_t bits;
-
-	std::memcpy(&bits, &f, sizeof(bits));
-
-	bits =  ((bits & 0x000000FFu) << 24) |
-			((bits & 0x0000FF00u) << 8)  |
-			((bits & 0x00FF0000u) >> 8)  |
-			((bits & 0xFF000000u) >> 24);
-
-
-	std::memcpy(&f, &bits, sizeof(bits));
-
-	return f;
+    uint32_t x = std::bit_cast<uint32_t>(f);
+    x = std::byteswap(x);
+    return std::bit_cast<float>(x);
 }
 
 inline void write(std::ofstream& file, const float data[], size_t N)
 {
-	if (is_little_endian()) {
-		file.write(reinterpret_cast<const char*>(data), sizeof(float) * N);
-	}
-	else {
-		for (size_t i=0; i < N; i++)
-		{
-			float f = reverse_bytes(data[i]);
-			file.write(reinterpret_cast<const char*>(&f), sizeof(float));
-		}
-	}
+    if (std::endian::native == std::endian::little)
+    {
+        file.write(reinterpret_cast<const char*>(data), sizeof(float) * N);
+    }
+    else 
+    {
+        for (size_t i=0; i < N; i++)
+        {
+            float f = reverse_bytes(data[i]);
+            file.write(reinterpret_cast<const char*>(&f), sizeof(float));
+        }
+    }
 }
 
 inline void read(std::ifstream& file, float data[], size_t N)
 {
-	file.read(reinterpret_cast<char*>(data), sizeof(float) * N);
+    file.read(reinterpret_cast<char*>(data), sizeof(float) * N);
 
-	if(!is_little_endian())
-	{
-		for (size_t i=0; i < N; i++)
-		{
-			data[i] = reverse_bytes(data[i]);
-		}
-	}
+    if (std::endian::native == std::endian::big)
+    {
+        for (size_t i=0; i < N; i++)
+        {
+            data[i] = reverse_bytes(data[i]);
+        }
+    }
 }
 
 #endif // IO_HPP
