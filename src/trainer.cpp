f0e7126725f27f57ea30c9e3077aa669d046b81c
Ugwu Valentine <ugwuvalentine262@gmail.com>
2026-06-14 13:29:35 -0600

    weights should be initialied from ~U(-0.5, 0.5)

--- src/trainer.cpp
+++ src/trainer.cpp
@@ -157,7 +157,7 @@ trainer::trainer(
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<float> dist(-1, 1);
+	std::uniform_real_distribution<float> dist(-0.5, 0.5);
 
 	for (size_t i=0; i < workers; i++)
 	{
